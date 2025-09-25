230c84debdb3530fd73a9ffaab67930ce8de80a6
OKELLO20 <jimokello31@gmail.com>
2025-09-25 19:20:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import openpyxl
 import time
 
-url = "https://api.aviationstack.com/v1/timetable?iataCode=NBO&type=arrival&access_key=88379c2b4cd1652465f13029212c63a6"
+#some parts of this code have been ommitted and will be made available once the developement of the application is complited
 
 headers = {
     'Accept': 'application/json',
@@ -14,15 +14,15 @@ headers = {
 
 response = requests.request("GET", url,headers=headers,data={})
 myjson = response.json()
-ourdata=[]
-csvheader = ['Arrival_time', 'airline_name']
+csvheader = ['Departure_time', 'airline_name']
+
 
 for x in myjson['data']:
     listing = [x['arrival']['actualTime'], x['airline']['name']]
     ourdata.append(listing)
-print('DAN DA DAN')
+print('Done')
 
-with open('Users/Phanice/Desktop/AviationData/quil.csv','w', encoding='UTF8', newline='') as f:
+with open('Users/[]/Desktop/AviationData/quil.csv','w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     writer.writerow(csvheader)
@@ -30,17 +30,17 @@ with open('Users/Phanice/Desktop/AviationData/quil.csv','w', encoding='UTF8', ne
 
 print('done')
 
-csv= pd.read_csv('Users/Phanice/Desktop/AviationData/quil.csv')
+csv= pd.read_csv('Users/[]/Desktop/AviationData/quil.csv')
 
-excelWriter = pd.ExcelWriter('Users/Phanice/Desktop/AviationData/aviation.xlsx')
+excelWriter = pd.ExcelWriter('Users/[]/Desktop/AviationData/aviation.xlsx')
 csv.to_excel(excelWriter)
 
 excelWriter.close()
-print('Dan Da Dan')
+print('Done')
 
 from openpyxl import load_workbook
 
-wb = load_workbook('Users/Phanice/Desktop/AviationData/aviation.xlsx')
+
 ws = wb.active
 
 ws.insert_cols(2)
@@ -50,8 +50,8 @@ ws.insert_cols(3)
 ws['C1'].value='Operation'
 
 
-excel_file = 'Users/Phanice/Desktop/AviationData/aviation.xlsx'
-df = pd.read_excel(excel_file, usecols=[0])
+excel_file = 'Users/[]/Desktop/AviationData/aviation.xlsx'
+
 
 leg = df.max()
 
@@ -60,13 +60,12 @@ x = int(leg) + 3
 for i in range(2,x):
     ws.cell(row=i, column=2).value = 'Nairobi'
 
-wb.save('Users/Phanice/Desktop/AviationData/aviation.xlsx')
 
 
 for i in range(2,x):
     ws.cell(row=i, column=3).value = 'Arrival'
 
-wb.save('Users/Phanice/Desktop/AviationData/aviation.xlsx')
+
 
 print('sleeping')
 
@@ -74,13 +73,9 @@ time.sleep(62)
 
 print('working')
 
-import requests
-import csv
-import pandas as pd
-import os
 
 
-url = "https://api.aviationstack.com/v1/timetable?iataCode=NBO&type=departure&access_key=88379c2b4cd1652465f13029212c63a6"
+url = "[]"
 
 headers = {
     'Accept': 'application/json',
@@ -89,15 +84,14 @@ headers = {
 
 response = requests.request("GET", url,headers=headers,data={})
 ourjson = response.json()
-ourdata=[]
 csvheader = ['Departure_time', 'airline_name']
 
 for x in ourjson['data']:
     listing = [x['departure']['actualTime'], x['airline']['name']]
     ourdata.append(listing)
-print('DAN DA DAN')
+print('Done')
 
-with open('Users/Phanice/Desktop/AviationData/third.csv','w', encoding='UTF8', newline='') as f:
+with open('Users/[]/Desktop/AviationData/third.csv','w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     writer.writerow(csvheader)
@@ -105,17 +99,14 @@ with open('Users/Phanice/Desktop/AviationData/third.csv','w', encoding='UTF8', n
 
 print('done')
 
-csv= pd.read_csv('Users/Phanice/Desktop/AviationData/third.csv')
 
-excelWriter = pd.ExcelWriter('Users/Phanice/Desktop/AviationData/flying.xlsx')
+excelWriter = pd.ExcelWriter('Users/[]/Desktop/AviationData/flying.xlsx')
 csv.to_excel(excelWriter)
 
 excelWriter.close()
-print('Dan Da Dan')
+print('Done')
 
-from openpyxl import load_workbook
 
-wb = load_workbook('Users/Phanice/Desktop/AviationData/flying.xlsx')
 ws = wb.active
 
 ws.insert_cols(2)
@@ -125,7 +116,7 @@ ws.insert_cols(3)
 ws['C1'].value='Operation'
 
 
-excel_file = 'Users/Phanice/Desktop/AviationData/flying.xlsx'
+excel_file = 'Users/[]/Desktop/AviationData/flying.xlsx'
 df = pd.read_excel(excel_file, usecols=[0])
 
 leg = df.max()
@@ -135,15 +126,12 @@ x = int(leg) + 3
 for i in range(2,x):
     ws.cell(row=i, column=2).value = 'Nairobi'
 
-wb.save('Users/Phanice/Desktop/AviationData/flying.xlsx')
 
 
-for i in range(2,x):
-    ws.cell(row=i, column=3).value = 'Departure'
 
-wb.save('Users/Phanice/Desktop/AviationData/flying.xlsx')
+wb.save('Users/[]/Desktop/AviationData/flying.xlsx')
 
-data_folder= '/Users/Phanice/Desktop/AviationData'
+data_folder= '/Users/[]/Desktop/AviationData'
 
 df = []
 
@@ -152,6 +140,5 @@ for file in os.listdir(data_folder):
         print('loading file {0}...'.format(file))
         df.append(pd.read_excel(os.path.join(data_folder,file)))
 
-df_master = pd.concat(df)
 df_master.to_excel('master file.xlsx', index=False)
 
